6f32ecc775cb7b58a01c924f404da2e3a213b36d
ScholaD <ifeomaegbeonu@gmail.com>
2023-10-12 07:41:43 -0700

    Airbnb

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -14,7 +14,7 @@ class FileStorage:
     __file_path = "file.json"
     __objects = {}
 
-     def all(self):
+    def all(self):
         """ return dictionary objects """
         return FileStorage.__objects
 
@@ -22,3 +22,25 @@ class FileStorage:
         """Set in __objects obj with key <obj_class_name>.id"""
         newname = obj.__class__.__name__
         FileStorage.__objects["{}.{}".format(newname, obj.id)] = obj
+
+    def save(self):
+         """ serializes objectss to the JSON file """
+         transformed = {}
+         for key in self.__objects:
+             object_details = self.__objects[key].to_dict()
+             transformed[key] = object_details
+         with open(self.__file_path, mode="w") as f:
+             json.dump(transformed, f)
+    
+    def reload(self):
+        """Deserializes data from a JSON file into __objects if the file exists."""
+
+        try:
+            with open(self.__file_path, mode="r") as f:
+                readed = json.load(f)
+            for key, dict_readed in readed.items():
+                 class_name = dict_readed['__class__']
+                 new_object = eval(class_name)(**dict_readed)
+                 self.new(new_object)
+        except FileNotFoundError:
+            return
