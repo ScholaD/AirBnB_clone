d1c9d271390d1f324244dbc6e869286dfe43d65c
0ade06 <abdurrahman.adenowo06@gmail.com>
2023-10-13 18:55:52 -0400

    compleated the console

--- console.py
+++ console.py
@@ -5,13 +5,23 @@ doc
 import cmd
 from models import storage
 from models.base_model import BaseModel
+from models.user import User
+from models.state import State
+from models.city import City
+from models.place import Place
+from models.amenity import Amenity
+from models.review import Review
+
 
 class HBNBCommand(cmd.Cmd):
     """
     the console for runing commands
     """
     prompt = "(hbnb)"
-    __available_class = ["BaseModel", "User"]
+    __available_class = [
+            "BaseModel", "User", "State", "City", "Amenity",
+            "Place", "Review"
+            ]
 
     def do_mytest_(self, line):
         Samp = storage.all()
@@ -38,7 +48,7 @@ class HBNBCommand(cmd.Cmd):
         elif line not in HBNBCommand.__available_class:
             print("** class doesn't exist **")
         else:
-            new_inst = BaseModel()
+            new_inst = eval(line)()
             new_inst.save()
             print(new_inst.id)
 
@@ -65,7 +75,6 @@ class HBNBCommand(cmd.Cmd):
             if flag == 0:
                 print("** no instance found **")
 
-
     def do_destroy(self, line):
         """
         Deletes an instance based on the class name and id
@@ -85,12 +94,10 @@ class HBNBCommand(cmd.Cmd):
                 if "{}.{}".format(lines[0], lines[1]) == a:
                     flag = 1
                     del obj_a[a]
-                    obj_a.save
-                    # not compleat ??????????????????????????????
-                    
+                    storage.save()
+                    break
             if flag == 0:
                 print("** no instance found **")
-        
 
     def do_all(self, line):
         """
@@ -117,15 +124,15 @@ class HBNBCommand(cmd.Cmd):
             obj_a = storage.all()
             new_l = []
             for b in obj_a.keys():
-                if "{}".format(line)== b.split('.')[0]:
-                          new_l.append(obj_a[b])
+                if "{}".format(line) == b.split('.')[0]:
+                    new_l.append(obj_a[b])
             print('[', end='')
             for a in new_l:
                 flag += 1
                 if flag < len(new_l):
                     print('"{}"'.format(a), end=', ')
                 else:
-                    print('"{}"'.format(a, end=''))
+                    print('"{}"'.format(a), end='')
             print(']')
 
     def do_update(self, line):
@@ -137,7 +144,34 @@ class HBNBCommand(cmd.Cmd):
         Example: $ update BaseModel 1234-1234-1234 email \"aibnb@email.com\"
         =====================================================================
         """
-        pass
+        flag = 0
+        lines = line.split()
+        if not line:
+            print("** class name missing **")
+        elif lines[0] not in HBNBCommand.__available_class:
+            print("** class doesn't exist **")
+        elif len(lines) < 2:
+            print("** instance id missing **")
+        else:
+            obj_a = storage.all()
+            for a in obj_a.keys():
+                if "{}.{}".format(lines[0], lines[1]) == a:
+                    flag = 1
+                    if len(lines) < 3:
+                        print("** attribute name missing **")
+                        break
+                    elif len(lines) < 4:
+                        print("** value missing **")
+                        break
+                    else:
+                        obj_c = obj_a[a]
+                        if lines[2] not in ['id', 'created_at', 'updated_at']:
+                            if lines[3][0] == '"' and lines[3][-1] == '"':
+                                setattr(obj_c, lines[2], lines[3][1:-1])
+                                storage.save()
+                        break
+            if flag == 0:
+                print("** no instance found **")
 
 
 if __name__ == "__main__":
