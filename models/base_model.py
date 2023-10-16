f09c4c0cbbdced4b99872896e6d6b30adae666c8
0ade06 <abdurrahman.adenowo06@gmail.com>
2023-10-16 05:54:21 -0400

    corrected the pycodestyle error

--- models/base_model.py
+++ models/base_model.py
@@ -4,6 +4,7 @@ import models
 from uuid import uuid4
 from datetime import datetime
 
+
 class BaseModel:
     """construct"""
 
@@ -19,21 +20,23 @@ class BaseModel:
                 if key == '__class__':
                     continue
                 if key == "created_at" or key == "updated_at":
-                      setattr(self, key, datetime.strptime(value, dformat))
+                    setattr(self, key, datetime.strptime(value, dformat))
                 else:
                     self.__dict__[key] = value
         else:
             models.storage.new(self)
 
     def save(self):
-        """  updates the updated_at attribute with the current datetime when called. """
+        """
+        updates the updated_at attribute with the current datetime when called.
+        """
 
         self.updated_at = datetime.today()
         models.storage.save()
 
     def to_dict(self):
         """  returns a dictionary representation of the object
-        with the class name added as __class__. It also converts the 
+        with the class name added as __class__. It also converts the
         created_at and updated_at attributes to ISO format strings """
 
         obj_dict = self.__dict__.copy()
@@ -41,9 +44,11 @@ class BaseModel:
         obj_dict['created_at'] = self.created_at.isoformat()
         obj_dict['updated_at'] = self.updated_at.isoformat()
         return obj_dict
-        
+
     def __str__(self):
-        """ Return string reprsentation of the object in the desired format. """
+        """
+        Return string reprsentation of the object in the desired format.
+        """
 
-        return "[{}] ({}) {}".format(self.__class__.__name__, self.id, self.__dict__)
-
+        return "[{}] ({}) {}".format(
+                self.__class__.__name__, self.id, self.__dict__)
--- models/city.py
+++ models/city.py
@@ -2,8 +2,9 @@
 """Defines the City class."""
 from models.base_model import BaseModel
 
+
 class City(BaseModel):
     """ City class that inherits BaseModel """
-    
+
     state_id = ""
     name = ""
