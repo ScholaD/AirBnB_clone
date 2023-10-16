e96d4c6168f8ce996eaef22c1a747c02f94d8c53
ScholaD <ifeomaegbeonu@gmail.com>
2023-10-16 04:11:09 -0700

    Airbnb

--- models/__init__.py
+++ models/__init__.py
@@ -2,5 +2,6 @@
 """ init for class FileStorage """
 from models.engine.file_storage import FileStorage
 
+
 storage = FileStorage()
 storage.reload()
--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -12,36 +12,32 @@ from models.review import Review
 
 class FileStorage:
     """ construct """
-<<<<<<< HEAD
 
-=======
->>>>>>> f09c4c0cbbdced4b99872896e6d6b30adae666c8
-    __file_path = "file.json"
-    __objects = {}
+    def __init__(self):
+        self.__file_path = "file.json"
+        self.__objects = {}
 
     def all(self):
         """ return dictionary objects """
-        return FileStorage.__objects
+        return self.__objects
 
     def new(self, obj):
         """Set in __objects obj with key <obj_class_name>.id"""
         newname = obj.__class__.__name__
-        FileStorage.__objects["{}.{}".format(newname, obj.id)] = obj
+        self.__objects["{}.{}".format(newname, obj.id)] = obj
 
     def save(self):
         """ serializes objectss to the JSON file """
         transformed = {}
-<<<<<<< HEAD
-        for key in FileStorage.__objects:
-            object_details = FileStorage.__objects[key].to_dict()
+        for key in self.__objects:
+            object_details = self.__objects[key].to_dict()
             transformed[key] = object_details
-        with open(FileStorage.__file_path, mode="w") as f:
+        with open(self.__file_path, mode="w") as f:
             json.dump(transformed, f)
 
     def reload(self):
         """Deserializes data from a
         JSON file into __objects if the file exists."""
-=======
         for key in self.__objects:
             object_details = self.__objects[key].to_dict()
             transformed[key] = object_details
@@ -52,11 +48,10 @@ class FileStorage:
         """
         Deserializes data from a JSON file into __objects if the file exists.
         """
->>>>>>> f09c4c0cbbdced4b99872896e6d6b30adae666c8
 
         try:
-            with open(FileStorage.__file_path, mode="r") as f:
-                readed = json.load(f)
+            with open(self.__file_path, mode="r") as f:
+                dict_readed = json.load(f)
             for key, dict_readed in readed.items():
                 class_name = dict_readed['__class__']
                 new_object = eval(class_name)(**dict_readed)
